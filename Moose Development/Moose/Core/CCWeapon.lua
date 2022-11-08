f8936d2f77cbaf78ba6311562a5bb56ab1f8064a
Niels <nielsvaes@gmail.com>
2022-11-08 09:11:06 +0100

    adding some properties to CCWEAPON

--- Moose Development/Moose/Core/CCWeapon.lua
+++ Moose Development/Moose/Core/CCWeapon.lua
@@ -7,10 +7,14 @@ do
     function CCWEAPON:New(dcs_object)
         self = BASE:Inherit(self, BASE:New())
         self.dcs_object = dcs_object
+        self.id = tostring(dcs_object.id_)
         self.fired_by = nil
         self.release_heading = nil
         self.release_altitude = nil
         self.release_coordinate = nil
+        self.last_heading = nil
+        self.last_velocity = nil
+        self.type = self.dcs_object:getTypeName()
         self.last_coordinate = nil
         self.impact_coordinate = nil
         self.impact_coordinate_terrain = nil
@@ -28,12 +32,46 @@ do
         return self.last_coordinate
     end
 
+    function CCWEAPON:GetHeading()
+        if not self:IsAlive() then
+            self:E("Can't get coordinate because weapon no longer exists!")
+            return
+        end
+        self.last_heading = UTILS.VecHdg(self.dcs_object:getVelocity())
+        return self.last_heading
+    end
+
+    function CCWEAPON:GetVelocity()
+        if not self:IsAlive() then
+            self:E("Can't get coordinate because weapon no longer exists!")
+            return
+        end
+        self.last_velocity = self.dcs_object:getVelocity()
+        return self.last_velocity
+    end
+
     function CCWEAPON:IsAlive()
         return self.dcs_object:isExist()
     end
 
     function CCWEAPON:GetLastCoordinate()
         return self.last_coordinate
+    end
+
+    function CCWEAPON:GetLastHeading()
+        return self.last_heading
+    end
+
+    function CCWEAPON:GetLastVelocity()
+        return self.last_velocity
+    end
+
+    function CCWEAPON:GetType()
+        return self.type
+    end
+
+    function CCWEAPON:GetID()
+        return self.id
     end
 end
 
