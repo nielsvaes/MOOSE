1972f3ba90b63af353b3463a042a72be6af02b2c
Niels Vaes <nielsvaes@gmail.com>
2024-05-15 16:49:30 +0200

    cleaner in log

--- Moose Development/Moose/Extras/Rule.lua
+++ Moose Development/Moose/Extras/Rule.lua
@@ -110,12 +110,14 @@ function RULE_MANAGER:Get()
 end
 
 function RULE_MANAGER:TestAllRules()
+    self:I("\n\n------------------- RULE TESTING-------------------")
     for _, rule in pairs(self.rules) do
         local test_table = {
             ["dev_condition"] = rule.dev_condition,
             ["f_condition  "] = rule.f_condition,
             ["f_action     "] = rule.f_action
         }
+
 
         for name, func in pairs(test_table) do
             local succeeded, return_value = pcall(func)
@@ -125,6 +127,7 @@ function RULE_MANAGER:TestAllRules()
             self:I(string.format("%s: %s [%s] - [%s]", rule:GetName(), name, tostring(succeeded), tostring(return_value)))
         end
     end
+    self:I("\n\n------------------- RULE TESTING-------------------\n\n")
 end
 
 function RULE_MANAGER:Add(rule)
