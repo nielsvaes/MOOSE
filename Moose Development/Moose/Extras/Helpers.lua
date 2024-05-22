ab4f904281762e0ed510736fc0ef4dc5beea48a8
Niels Vaes <nielsvaes@gmail.com>
2024-05-22 23:10:25 +0200

    non idiotic userflag checking

--- Moose Development/Moose/Extras/Helpers.lua
+++ Moose Development/Moose/Extras/Helpers.lua
@@ -13,7 +13,9 @@ function dev_message(message, time_delay)
     end
 
     if time_delay then
-        delay(time_delay, function() trigger.action.outText("[DEV: " .. message .. "]", 10)  end)
+        delay(time_delay, function()
+            trigger.action.outText("[DEV: " .. message .. "]", 10)
+        end)
         BASE:I("[DEV: " .. message .. "]")
     else
         trigger.action.outText("[DEV: " .. message .. "]", 10)
@@ -25,7 +27,6 @@ function delay(t, f)
     BASE:ScheduleOnce(t, f)
 end
 
-
 function ask_user(text, flagname_true, flagname_false, usersound)
     if usersound then
         usersound:ToAll()
@@ -35,7 +36,6 @@ function ask_user(text, flagname_true, flagname_false, usersound)
     local question = [[c_start_wait_for_user("]] .. flagname_true .. [[", "]] .. flagname_false .. [[")]]
     net.dostring_in('mission', question)
 end
-
 
 function all_of_group_in_zone(group, zone)
     if type(zone) == "string" then
@@ -71,22 +71,18 @@ function part_of_group_in_zone(group, zone)
     return false
 end
 
-
-
---- Extra USERFLAG
-
-function USERFLAG:Get(flag_name)
+function get_userflag(flag_name)
     return trigger.misc.getUserFlag(flag_name)
 end
 
-function USERFLAG:Set(flag_name, value)
+function set_userflag(flag_name, value)
     if value == false then
         value = 0
     end
     trigger.action.setUserFlag(flag_name, value)
 end
 
-function USERFLAG:Equals(flag_name, value)
+function userflag_equals(flag_name, value)
     if value == true then
         value = 1
     elseif value == false then
