1e2c8df80bd5f83f44ebbd8e9a0b899bcaa257d0
Niels Vaes <nielsvaes@gmail.com>
2024-06-03 20:23:20 +0200

    adding clear to dev_message DMS_to_DD DMS_to_coordinate table.compare

--- Moose Development/Moose/Extras/Helpers.lua
+++ Moose Development/Moose/Extras/Helpers.lua
@@ -7,18 +7,19 @@ end
 
 --- One liners
 
-function dev_message(message, time_delay)
+function dev_message(message, clear, time_delay)
     if not DEVCODE then
         return
     end
+    clear = clear or false
 
     if time_delay then
         delay(time_delay, function()
-            trigger.action.outText("[DEV: " .. message .. "]", 10)
+            trigger.action.outText("[DEV: " .. message .. "]", 10, clear)
         end)
         BASE:I("[DEV: " .. message .. "]")
     else
-        trigger.action.outText("[DEV: " .. message .. "]", 10)
+        trigger.action.outText("[DEV: " .. message .. "]", 10, clear)
         BASE:I("[DEV: " .. message .. "]")
     end
 end
@@ -108,4 +109,53 @@ function go_to_waypoint(group, waypoint)
     waypoint = waypoint + 1
     local cmd = group:CommandSwitchWayPoint(1, waypoint)
     group:SetCommand(cmd)
+end
+
+function DMS_to_DD(coord) -- input like "N31*50.800" or "E036*46.200"
+    -- Adjust the pattern to match coordinates without a space after the direction
+    local direction, degrees, minutes = coord:match("([NSEW])(%d+)%*(%d+%.%d+)")
+
+    -- Convert degrees and minutes to numbers
+    degrees = tonumber(degrees)
+    minutes = tonumber(minutes)
+
+    -- Calculate decimal degrees
+    local decimal_degrees = degrees + (minutes / 60)
+
+    -- Adjust for direction, assuming North and East are positive, South and West are negative
+    if direction == 'S' or direction == 'W' then
+        decimal_degrees = -decimal_degrees
+    end
+
+    return decimal_degrees
+end
+
+function DMS_to_coordinate(lat, lon)
+    dec_lat = DMS_to_DD(lat)
+    dec_lon = DMS_to_DD(lon)
+    BASE:I(coord.LLtoLO(dec_lat, dec_lon))
+    return COORDINATE:NewFromVec3(coord.LLtoLO(dec_lat, dec_lon))
+end
+
+function table.compare(t1, t2, ignore_mt)
+    local ty1 = type(t1)
+    local ty2 = type(t2)
+    if ty1 ~= ty2 then return false end
+    if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+    local mt = getmetatable(t1)
+    if not ignore_mt and mt and mt.__eq then return t1 == t2 end
+    for k1, v1 in pairs(t1) do
+        local v2 = t2[k1]
+        if v2 == nil or not table.compare(v1, v2, ignore_mt) then return false end
+    end
+    for k2, v2 in pairs(t2) do
+        local v1 = t1[k2]
+        if v1 == nil or not table.compare(v1, v2, ignore_mt) then return false end
+    end
+    return true
+end
+
+
+function string.remove_last_characters(str, num_characters)
+    return str:sub(1, -num_characters - 1)
 end
